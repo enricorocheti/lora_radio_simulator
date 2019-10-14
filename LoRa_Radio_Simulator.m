92bae5ad6d71b40f66a88c8b4179e6f59e1aa36d
Enrico Oliveira Rocheti <e155249@dac.unicamp.br>
2019-10-14 20:47:10 -0300

    Probabilidade de erro de bit adicionada

--- LoRa_Radio_Simulator.m
+++ LoRa_Radio_Simulator.m
@@ -2,39 +2,56 @@ clc;
 clear all;
 close all;
 
-sigma=1;
-EsdB=[-30:-5];
-tam=1e3;
+sigma = 1;
+EsdB = [-25:0];
+size = 1e4;
 
 for SF=[7:1:10]
-    k=[0:2^SF-1];
+    k = [0:2^SF-1];
 
-    W=zeros(2^SF);
+    W = zeros(2^SF);
     for n=0:2^SF-1
-        W(n+1,:)=exp(sqrt(-1).*2.*pi.*mod(k+n,2^SF).*n./2^SF);
+        W(n+1,:) = exp(sqrt(-1).*2.*pi.*mod(k+n,2^SF).*n./2^SF);
     end
 
-    pbit=[];
-    Tx=[];
-    Rx=[];
+    prob_symbol_error = [];
+    prob_bit_error = [];
+    %pbit = zeros(1,length(EsdB));
+    Tx = [];
+    Rx = [];
     for Es=10.^(0.1.*EsdB)
-        vecerro=zeros(1,tam);
-        for i=1:tam
-            indice=randi(2^SF-1);
-            Tx=sqrt(Es/2^SF).*W(indice,:);    
-            ruido=sigma/sqrt(length(Tx))*(randn(1,length(Tx))+sqrt(-1)*randn(1,length(Tx)));    
-            h=abs(1/sqrt(2)*(randn(1,2^SF)+sqrt(-1)*randn(1,2^SF)));
-            Rx=Tx+ruido;    
-            %Rx=h.*Tx+ruido;    
-            [a,b]=max(abs(sqrt(Es/2^SF)*Rx*W'));
-            vecerro(i)=(b~=indice);
+        symbol_error = zeros(1,size);
+        bit_error = zeros(1,size);
+        for i=1:size
+            symbol = randi(2^SF-1);
+            Tx = sqrt(Es/2^SF).*W(symbol,:);    
+            noise = sigma/sqrt(length(Tx))*(randn(1,length(Tx))+sqrt(-1)*randn(1,length(Tx)));    
+            %h = abs(1/sqrt(2)*(randn(1,2^SF)+sqrt(-1)*randn(1,2^SF)));
+            Rx = Tx + noise;    
+            %Rx = h.*Tx + noise;    
+            [a,b] = max(abs(sqrt(Es/2^SF)*Rx*W'));
+            symbol_error(i) = (b~=symbol);
+            bit_error(i) = get_bit_error(symbol,b,SF);
         end
-        pbit=[pbit sum(vecerro)/(tam)]
+        prob_symbol_error = [prob_symbol_error sum(symbol_error)/(size)];
+        prob_bit_error = [prob_bit_error sum(bit_error)/(size*SF)];
     end
-    semilogy(EsdB,pbit)
+    figure(1)
+    semilogy(EsdB,prob_symbol_error)
+    hold on
+    figure(2)
+    semilogy(EsdB,prob_bit_error)
     hold on
 end
+
+figure(1)
 title('Probabilidade de Erro de Símbolo');
 ylabel('Psym');
+xlabel('Es [dB]');
+legend('SF = 7','SF = 8','SF = 9','SF = 10');
+
+figure(2)
+title('Probabilidade de Erro de Bit');
+ylabel('Pbit');
 xlabel('Es [dB]');
 legend('SF = 7','SF = 8','SF = 9','SF = 10');

5ef92d1ef323a1ff9f97c7590728481711a7685a
Enrico Oliveira Rocheti <enrico@timeenergy.com.br>
2019-12-03 09:51:12 -0300

    Reorganizando arquivos

--- Trabalho_2_LoRa/LoRa_Rx_v2.m
+++ Trabalho_2_LoRa/LoRa_Rx_v2.m
@@ -1,20 +1,28 @@
+% Recebe mensagem em formato .wave
+% [Rx,Fs] = audioread('gravacao.wav');
+% Rx = reshape(Rx,1,[]);
+
+
+% Recebe a mensagem via gravação de áudio
 recObj = audiorecorder(Fs, 24, 1);
 disp('Iniciando gravação do sinal de áudio');
 recordblocking(recObj, round(length(symbols_Tx).*Ts + 2));
 disp('Gravação finalizada');
 Rx = getaudiodata(recObj)';
 
-% [Rx,Fs] = audioread('gravacao_AGORAVAI.wav');
-% Rx = reshape(Rx,1,[]);
-
+% Retorna o sinal a banda base
 t_Rx = [0:1/Fs:(length(Rx)-1)/Fs];
 RF_Rx = exp(sqrt(-1).*2.*pi.*f.*t_Rx);
 Rx = hilbert(Rx).*(1./RF_Rx);
 
+% Encontra a posição da mensagem com base na sequência de sincronismo
 correlation = xcorr(mls_,Rx);
 [value,index_mls] = findpeaks(abs(correlation),'Npeaks',2,'SortStr','descend');
-%plot(real(correlation));
-%msg_size = abs(abs(index_mls(2)-index_mls(1))-length(mls_));
+%figure(2);
+%plot(abs(correlation));
+%msg_size = abs(abs(index_mls(2)-index_mls(1))-length(mls_));   
+% inicio_msg = length(Rx)-index_mls(2)+length(mls_)-1;
+% msg_decoded = Rx(inicio_msg:inicio_msg+msg_size-1);
 msg_size = Fs*length(symbols_Tx)*Ts;
 
 for i = 1:round(length(Rx)/2)
@@ -29,19 +37,18 @@ for i = 1:round(length(Rx)/2)
     if symbols_Rx == symbols_Tx
         mls_RX = Rx(i-length(mls_):i-1);
         sound(real(msg_decoded));
-        symbols_Rx
+        char(symbols_Rx)
         break
     end
 end 
 
 %% Quando eu encontrar o inicio da mensagem, utilizar este código
 
-% msg_decoded = Rx(inicio_msg:inicio_msg+msg_size);
+%msg_decoded = Rx(inicio_msg:inicio_msg+msg_size);
 % symbols_Rx = [];
 % for i = (1:Fs*Ts:length(msg_decoded))
 %      lora_block = msg_decoded(i:(i-1+Fs*Ts));
 %      [a,b] = max(abs(sqrt(Es/2^SF)*lora_block*W'));
 %      symbols_Rx = [symbols_Rx b-1];
 % end
-% symbols_Rx
-
+% char(symbols_Rx)
--- Trabalho_2_LoRa/LoRa_Tx_v2.m
+++ Trabalho_2_LoRa/LoRa_Tx_v2.m
@@ -1,33 +1,44 @@
-clc;
 clear all;
 close all;
   
-SF = 10;
-Es = 2^SF;
-k = 0:2^SF-1;
-f = 600;
-Fs = 8000;
-Ts = 0.1; % Tempo de um símbolo apenas
-t = [0:1/Fs:Ts-1/Fs];
-symbols_Tx = [50 100 150 200];
+SF = 10;                    % Spreading Factor
+Es = 2^SF;                  % Energia de símbolo LoRa
+f = 600;                    % Freq. da portada
+Fs = 8000;                  % Freq. de amostragem do áudio
+Ts = 1;                     % Tempo de símbolo LoRa
+B = 2^SF/Ts;                % Banda do sinal LoRa
 
+t = [0:1/Fs:Ts-1/Fs];
+k = 0:2^SF-1;               
+
+% Mensagem do usuário
+string = input('Digite a frase a ser transmitida: ','s'); 
+symbols_Tx = double(string);
+
+% Base ortonormal que define o espaço de sinais LoRa
 W = zeros(2^SF,length(t));
 for k = 0:2^SF-1
 	W(k+1,:)=sqrt(Es/2^SF).*exp(sqrt(-1).*2.*pi.*t.*mod(linspace(k,k+2^SF,length(t)),2^SF));
 end
 
+% Formas de onda LoRa de acordo com os símbolos que desejam ser transmitidos
 Tx = [];
 for symbol = symbols_Tx
     Tx = [Tx W(symbol+1,:)];
 end
+lora_wave = Tx;
+figure(1);
+plot(real(lora_wave(1:5:end)));
 
+% Adiciona a sequência de sincronismo no início e no fim da mensagem
 mls_ = repelem(mls(8,1),5);
-Tx_AUX = Tx;
 Tx = [mls_ Tx mls_];
 
-t2 = [0:1/Fs:(length(Tx)-1)/Fs];
-RF = exp(sqrt(-1).*2.*pi.*f.*t2);
-Tx = Tx.*RF;
+% Passa o sinal para freq. audível de acordo com f defino no início do programa
+t_Tx = [0:1/Fs:(length(Tx)-1)/Fs];
+RF_Tx = exp(sqrt(-1).*2.*pi.*f.*t_Tx);
+Tx = Tx.*RF_Tx;
 
+% Transmite a mensagem em formato de som
 sound(real(Tx),Fs);
-audiowrite('gravacao_AGORAVAI.wav',real(Tx),Fs);
+audiowrite('mensagem.wav',real(Tx),Fs);
